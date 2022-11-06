47b1dbbeef51b178185ee236fa0cf53b03c86771
salehmu <ghd@keemail.me>
2022-11-06 14:05:00 +0200

    init C# dummy example

---
+++ DummyCsharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Csharp_Services;
+using Grpc.Net.Client;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+
+using var channel = GrpcChannel.ForAddress("http://localhost:4040");
+var client = new RouteGuide.RouteGuideClient(channel);
+var myhello = client.GetHello(new Hello() {Hello_ = "Csharp"});
+Console.Write(myhello.Hello_);
